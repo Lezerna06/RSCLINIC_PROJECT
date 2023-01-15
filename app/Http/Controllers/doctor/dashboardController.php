8cb3481cae79db3c7df5e8bbfc59eeb625399b2f
Kimberly Nineria <115699495+KimberlyNineria@users.noreply.github.com>
2023-01-15 21:01:57 +0800

    Update dashboardController.php

--- app/Http/Controllers/doctor/dashboardController.php
+++ app/Http/Controllers/doctor/dashboardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class dashboardController extends Controller
 {
     public function index(){
-        return view('doctor.dashboard');
+        return view('doctors.dashboard');
     }
 }
