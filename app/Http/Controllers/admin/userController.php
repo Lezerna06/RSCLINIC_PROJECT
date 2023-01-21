63b79288f595f0b706e62cbd33c4be6969b27ce9
edenflorendo <115640844+edenflorendo@users.noreply.github.com>
2023-01-21 11:37:27 +0800

    Update userController.php

--- app/Http/Controllers/admin/userController.php
+++ app/Http/Controllers/admin/userController.php
@@ -1,3 +1,21 @@
 <?php
 
 namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Userdata;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class userController extends Controller
+{
+    //
+    public function index(){
+        $data = [
+            'count' => 1,
+            'users' => User::with('userdata')->where('restriction',3)->orderBy('fullName', 'Asc')->get()
+        ];
+        return view('admin.user.index',$data);
+    }
+}
